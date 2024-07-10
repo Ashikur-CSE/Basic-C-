01b99fb0658a3532cbae615d10b205adacf02f65
Ashikur-CSE <ashikur201.cse@gmail.com>
2024-07-10 12:40:12 +0600

    All method

--- Count a char in a string.cpp
+++ Count a char in a string.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<algorithm>
 using namespace std;
 int main(){
     string text;
@@ -7,13 +8,27 @@ int main(){
     char charecter;
     cout<<"Charecter:";
     cin>>charecter;
-    int count=0;
+    int count1=0;
+
+    
+    // for (char c:text){
+    //     if(c==charecter){
+    //         count1+=1;
+    //     }
+    // }
+     
 
     for(int i=0;i<text.length();i++){
         if(text[i]==charecter){
-            count+=1;
+             count1+=1;
         }
     }
-    cout<<"Number of "<<charecter<<" is:"<<count;
+
+    // //using Algorithm 
+    //  long occurrences;
+    //  occurrences= count(text.begin(),text.end(),charecter);
+    //  cout<<"Number of "<<charecter<<" is:"<<occurrences;
+
+    cout<<"Number of "<<charecter<<" is:"<<count1;
 
 }
